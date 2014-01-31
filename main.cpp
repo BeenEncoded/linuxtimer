2f0809796fa85f22d9782562c32af4b708e3c79a
Jonathan <jonathancw@me.com>
2014-01-31 15:55:02 -0800

    Added timer.  Timer has been tested, and can be tested.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <unistd.h>
+#include <sstream>
 
 #include "cwrapper.hpp"
 #include "common.hpp"
+#include "timer_class.hpp"
 
 using namespace cwrap;
 
@@ -9,6 +12,58 @@ using namespace cwrap;
 /** Menu stuff: */
 namespace
 {
+    template<class type1, class type2>
+    inline type2 conv(const type1& t1)
+    {
+        std::stringstream ss;
+        type2 t2;
+        ss<< t1;
+        ss>> t2;
+        return t2;
+    }
+    
+    inline std::string formatted_number(const unsigned int& x, const short& digits)
+    {
+        std::string temps("");
+        short digits_of_x(conv<unsigned int, std::string>(x).size());
+        if(digits_of_x < digits)
+        {
+            for(short y = digits_of_x; y < digits; y++)
+            {
+                temps += "0";
+            }
+        }
+        temps += conv<unsigned int, std::string>(x);
+        return temps;
+    }
+    
+    inline void display_timer(const timerObj::time_info_data& timer, const bool& finished)
+    {
+        common::center(std::string(
+                (finished ? "OVERTIME: " : std::string("")) + 
+                formatted_number(timer.hours, 2) + " : " + 
+                formatted_number(timer.minutes, 2) + " : " +
+                formatted_number(timer.seconds, 2) + " : " +
+                formatted_number(timer.miliseconds, 2)));
+    }
+    
+    inline void test_timer()
+    {
+        using namespace timerObj;
+        using namespace common;
+        
+        timer_class timer;
+        timer.set(5);
+        timer.start();
+        while(!kbhit())
+        {
+            cls();
+            for(short x = 0; x < 10; x++) cout<< endl;
+            display_timer(timer.time_left(), timer.finished());
+            usleep((1000000 / 30));
+        }
+    }
+    
     inline void main_menu()
     {
         cout.set_color(white);
@@ -26,7 +81,7 @@ namespace
             center("UNDER CONSTRUCTION!");
             cout.set_color(white);
             cout<< endl;
-            cout<< " 1 -  Start timer (under construction!)"<< endl;
+            cout<< " 1 -  Start timer (under testing)"<< endl;
             cout<< " 2 -  Set timer (under construction!)"<< endl;
             cout<< endl;
             cout<< " q -  Exit"<< endl;
@@ -35,6 +90,7 @@ namespace
             {
                 case '1':
                 {
+                    test_timer();
                 }
                 break;
                 
---
+++ timer_class.hpp
@@ -0,0 +1,162 @@
+#ifndef TIMER_CLASS_HPP_INCLUDED
+#define TIMER_CLASS_HPP_INCLUDED
+#include <string>
+#include <unistd.h>
+#include <time.h>
+#include <fstream>
+
+namespace timerObj
+{
+    class clock_class;
+    class timer_class;
+    
+    struct time_info_data;
+    
+    struct time_info_data
+    {
+        unsigned int hours, minutes, seconds, miliseconds;
+    };
+    
+    std::ifstream& operator>>(std::ifstream&, clock_class&);
+    std::ofstream& operator<<(std::ofstream&, clock_class&);
+    
+    class clock_class
+    {
+    public:
+        friend std::ifstream& operator>>(std::ifstream&, clock_class&);
+        friend std::ofstream& operator<<(std::ofstream&, clock_class&);
+        
+        explicit clock_class() : t(), delim(2) {}
+        explicit clock_class(const long& ct) : t(), delim(2)
+        {
+            this->t.tv_nsec = ct;
+        }
+        
+        ~clock_class(){}
+        
+        clock_class operator=(const clock_class& cc)
+        {
+            if(this != &cc)
+            {
+                this->t.tv_nsec = cc.t.tv_nsec;
+                this->t.tv_sec = cc.t.tv_sec;
+            }
+            return *this;
+        }
+        
+        bool operator==(const clock_class& cc) const
+        {
+            return ((this->t.tv_nsec == cc.t.tv_nsec) && (this->t.tv_sec == cc.t.tv_sec));
+        }
+        
+        bool operator<(const clock_class& cc) const
+        {
+            return (this->t.tv_nsec < cc.t.tv_nsec);
+        }
+        
+        bool operator<=(const clock_class& cc) const
+        {
+            return (this->t.tv_nsec <= cc.t.tv_nsec);
+        }
+        
+        bool operator>(const clock_class& cc) const
+        {
+            return (this->t.tv_nsec > cc.t.tv_nsec);
+        }
+        
+        bool operator>=(const clock_class& cc) const
+        {
+            return (this->t.tv_nsec >= cc.t.tv_nsec);
+        }
+        
+        /* Returns the clock_t that the class is set to */
+        const long& raw() const
+        {
+            return this->t.tv_nsec;
+        }
+        
+        /* Returns the raw information calculated into
+         hours, minutes, seconds, and miliseconds. */
+        time_info_data gtime() const
+        {
+            unsigned long temp(this->t.tv_nsec), persec(1000000000);
+            time_info_data time_info;
+            
+            time_info.hours = (temp / persec / 60 / 60);
+            temp %= (persec * 60 * 60);
+            
+            time_info.minutes = (temp / persec / 60);
+            temp %= (persec * 60);
+            
+            time_info.seconds = (temp / persec);
+            temp %= persec;
+            
+            time_info.miliseconds = (temp / (persec / 100));
+            return time_info;
+        }
+        
+    private:
+        timespec t;
+        char delim;
+        
+    };
+    
+    
+    class timer_class
+    {
+    public:
+        explicit timer_class() : beg(0), 
+                seconds_set(0) {}
+        
+        ~timer_class(){}
+        
+        /* Starts the timer, by setting the beginning time. */
+        void start()
+        {
+            timespec tempt;
+            clock_gettime(CLOCK_REALTIME, &tempt);
+            this->beg = clock_class((tempt.tv_nsec + (tempt.tv_sec * (unsigned long)1000000000)));
+        }
+        
+        /* Sets the timer, in seconds. */
+        void set(const unsigned int& sec)
+        {
+            this->seconds_set = clock_class((sec * (unsigned long)1000000000));
+        }
+        
+        /* Returns the amount of time left on the clock.  If the end_time
+         has been passed, then it will start counting up.*/
+        time_info_data time_left() const
+        {
+            timespec tempt;
+            clock_gettime(CLOCK_REALTIME, &tempt);
+            unsigned long now((tempt.tv_nsec + (tempt.tv_sec * (unsigned long)1000000000))), end(this->end_time());
+            if(end < now)
+            {
+                std::swap(end, now);
+            }
+            return clock_class(end - now).gtime();
+        }
+        
+        /* Returns true if the timer has passed the end time.  This
+         allows it to count up from zero, once the end_time has been
+         reached, acting as an "overtime" clock.*/
+        bool finished() const
+        {
+            timespec tempt;
+            clock_gettime(CLOCK_REALTIME, &tempt);
+            return ((unsigned long)this->end_time() <= (tempt.tv_nsec + (tempt.tv_sec * (unsigned long)1000000000)));
+        }
+        
+    private:
+        clock_class beg, seconds_set;
+        
+        long end_time() const
+        {
+            return clock_t((this->beg.raw() + this->seconds_set.raw()));
+        }
+        
+    };
+}
+
+#endif
